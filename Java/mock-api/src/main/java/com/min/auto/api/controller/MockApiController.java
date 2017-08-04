fe237944fabb5803841a481f3be4d806db73baab
minyangcheng <332485508@qq.com>
2017-08-04 15:46:31 +0800

    修改数据返回格式

--- Java/mock-api/src/main/java/com/min/auto/api/controller/MockApiController.java
+++ Java/mock-api/src/main/java/com/min/auto/api/controller/MockApiController.java
@@ -1,8 +1,6 @@
 package com.min.auto.api.controller;
 
-import com.min.auto.api.bean.Result;
 import com.min.auto.api.service.MockApiService;
-import com.min.auto.api.util.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +18,9 @@ public class MockApiController {
     private MockApiService mockApiService;
 
     @RequestMapping("/**/*")
-    public Result mock(HttpServletRequest request, HttpServletResponse response){
-        String respData=mockApiService.getRespData(request,response);
-        return ResultUtil.success(respData);
+    public String mock(HttpServletRequest request, HttpServletResponse response) {
+        String respData = mockApiService.getRespData(request, response);
+        return respData;
     }
 
 }
