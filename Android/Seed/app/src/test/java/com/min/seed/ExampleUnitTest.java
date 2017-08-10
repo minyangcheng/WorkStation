09dd5fc2867ae795371d1cab15a2814434bb2ca5
minyangcheng <332485508@qq.com>
2017-08-10 19:40:20 +0800

    添加种子项目

---
+++ Android/Seed/app/src/test/java/com/min/seed/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package com.min.seed;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void additionIsCorrect() throws Exception {
+        Assert.assertEquals(4, 2 + 2);
+    }
+}
